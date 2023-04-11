f87093043871eb117b6eb537378686759ac36cda
denpamusic <denpa@denpa.pro>
2023-04-11 03:35:47 +0300

    Improve reconnect callback.

--- custom_components/dlms_cosem/sensor.py
+++ custom_components/dlms_cosem/sensor.py
@@ -269,10 +269,19 @@ class CosemSensor(SensorEntity):
             response if response is None else self.entity_description.value_fn(response)
         )
 
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added to hass."""
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass, SIGNAL_RECONNECTED, self._reconnect_callback
+            )
+        )
+        await super().async_added_to_hass()
+
     @callback
-    def async_schedule_update_after_reconnect(self) -> None:
+    def _reconnect_callback(self) -> None:
         """Schedules update after the reconnect."""
-        self.async_schedule_update_ha_state(True)
+        self.async_schedule_update_ha_state(force_refresh=True)
 
     @property
     def available(self) -> bool:
@@ -287,14 +296,8 @@ async def async_setup_entry(
 ) -> bool:
     """Set up the sensor platform."""
     connection: DlmsConnection = hass.data[DOMAIN][config_entry.entry_id]
-    entities: list[CosemSensor] = [
-        CosemSensor(connection, description) for description in SENSOR_TYPES
-    ]
-
-    for entity in entities:
-        async_dispatcher_connect(
-            hass, SIGNAL_RECONNECTED, entity.async_schedule_update_after_reconnect
-        )
-
-    async_add_entities(entities, update_before_add=True)
+    async_add_entities(
+        [CosemSensor(connection, description) for description in SENSOR_TYPES],
+        update_before_add=True,
+    )
     return True
